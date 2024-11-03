5210c7380b3607c6afb71960b904a8e7f41ca6d9
huy <huy@fagmann.no>
2024-11-03 16:11:55 +0100

    Cleaner with only components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,17 @@
+import useCarHooks from "../hooks/useCarHooks";
+import Search from "./search/Search";
+import Vehicle from "./vehicle/Vehicle";
+
 const Home = (): JSX.Element => {
-	return <div>Home</div>;
+	const { vehicleData } = useCarHooks();
+
+	return (
+		<div>
+			<h1>Søk etter kjøretøy</h1>
+			<Search />
+			{vehicleData && <Vehicle vehicleData={vehicleData} />}
+		</div>
+	);
 };
 
 export default Home;
