e6b2073ffdd89118c085aaee0485e9e36450c440
huy <huy@fagmann.no>
2024-11-03 16:29:51 +0100

    Styling search component

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,4 +1,5 @@
 import useCarHooks from "../../hooks/useCarHooks";
+import "../../styles/search.scss";
 
 const Search = () => {
 	const { registrationNumber, fetchVehicleData, handleInputChange } =
@@ -6,13 +7,22 @@ const Search = () => {
 
 	return (
 		<form onSubmit={fetchVehicleData}>
-			<input
-				type="text"
-				value={registrationNumber}
-				onChange={handleInputChange}
-				placeholder="Registreringsnummer"
-			/>
-			<button type="submit">Hent kjøretøydata</button>
+			<div className="form-wrapper">
+				<div className="license-plate">
+					<div className="flag-label">
+						<img src="" alt="Norwegian flag" />
+						<label htmlFor="registrationNumber">N</label>
+					</div>
+					<input
+						type="text"
+						id="registrationNumber"
+						value={registrationNumber}
+						onChange={handleInputChange}
+						placeholder="Registreringsnummer"
+					/>
+				</div>
+				<button type="submit">Hent kjøretøydata</button>
+			</div>
 		</form>
 	);
 };
