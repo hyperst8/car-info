49e1e00f9a32b0905cfd531242f7674ccfb48457
huy <huy@fagmann.no>
2024-11-03 16:15:24 +0100

    Styling home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,13 @@
 import useCarHooks from "../hooks/useCarHooks";
 import Search from "./search/Search";
 import Vehicle from "./vehicle/Vehicle";
+import "../styles/home.scss";
 
 const Home = (): JSX.Element => {
 	const { vehicleData } = useCarHooks();
 
 	return (
-		<div>
+		<div className="home">
 			<h1>Søk etter kjøretøy</h1>
 			<Search />
 			{vehicleData && <Vehicle vehicleData={vehicleData} />}
